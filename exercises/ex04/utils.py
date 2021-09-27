764da21da6f4d6b4d5e6dde012fb932fe2570003
Sophia <sophcarr@live.unc.edu>
2021-09-27 11:46:27 -0400

    progress on ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,45 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730320301"
 
 
 # TODO: Implement your functions here.
+def all(haystack: list[int], needle: int) -> bool:
+    i: int = 0
+    total: int = 0
+    while i < len(haystack):
+        if haystack[i] == needle:
+            total += 1
+        i += 1
+    if total == len(haystack):
+        return True
+    else:
+        return False
+
+
+def is_equal(x: list[int], y: list[int]) -> bool:
+    i: int = 0
+    total: int = 0
+    if len(x) != len(y):
+        return False
+    while i < len(y):
+        if y[i] == x[i]:
+            total += 1
+        i += 1
+    if total == len(x):
+        return True
+    else:
+        return False
+
+
+def max(input: list[int]) -> int:
+    i: int = 0
+    max: int = input[len(input) - 1]
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty list")
+    while i < len(input):
+        first: int = input[i]
+        if first > max:
+            max = first
+        i += 1
+    return max
