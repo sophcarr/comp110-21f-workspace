4008a51b257db90e3bd4f5a8ad3dc55b93565119
Sophia <sophcarr@live.unc.edu>
2021-10-02 20:32:00 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,44 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730320301"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Finding the even numbers in a list."""
+    i: int = 0
+    while i < len(xs):
+        if xs[i] % 2 != 0:
+            xs.pop(i)
+        i += 1
+    return xs
+
+
+def sub(a_list: list[int], y: int, z: int) -> list[int]:
+    """Generating a list from a list."""
+    final: list[int] = list()
+    i: int = 1
+    while len(a_list) == 0 or y == 0 or z <= 0:
+        return final
+    if y < 0:
+        final.append(a_list[0])
+    while i < (len(a_list) - 1):
+        final.append(a_list[i])
+        i += 1
+    if z > len(a_list):
+        final.append(a_list[len(a_list) - 1])
+    return final
+
+
+def concat(x: list[int], y: list[int]) -> list[int]:
+    final: list[int] = list()
+    i: int = 0
+    s: int = 0
+    while i < len(x):
+        final.append(x[i])
+        i += 1
+    while s < len(y):
+        final.append(y[s])
+        s += 1
+    return final
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,30 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+__author__ = "730320301"
+
+
+def test_sub_edge() -> None:
+    """Testing edge case for sub when y = 0."""
+    a_list: list[float] = [1, 2, 3, 4, 5, 6]
+    x: int = 3
+    y: int = 0
+    assert final == []
+
+
+def test_sub() -> None:
+    """Testing sub."""
+    a_list: list[float] = [1, 2, 3, 4, 5, 6]
+    x: int = 3
+    y: int = 1
+    assert list(final) == [2, 4, 6]
+
+
+def test_concat() -> None:
+    """Testing concat."""
+    x: list[float] = [1, 2, 3, 4, 5, 6]
+    y: list[float] = [555, 33, 1353]
+    assert list(final) == [2, 4, 6]
