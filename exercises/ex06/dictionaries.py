dd5081407a498f876975a7babd64bb274ddc442a
Sophia <sophcarr@live.unc.edu>
2021-10-13 14:05:50 -0400

    Progress on ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,41 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730320301"
 
 # Define your functions below
+
+
+def invert(list1: dict[str, str]) -> dict[str, str]:
+    """Switching keys with thier corresponding values."""
+    list2: dict[str, str] = dict()
+    for key in list1:
+        list2[list1[key]] = key
+    return list2
+
+
+def favorite_color(list1: dict[str, str]) -> str:
+    """Counting most popular color."""
+    zzz: dict[str, int] = dict()
+    max: int = 0
+    final: str = ""
+    for key in list1:
+        if list1[key] in zzz:
+            zzz[list1[key]] += 1
+        else:
+            zzz[list1[key]] = 1
+    for key in zzz:
+        first: int = zzz[key]
+        if first > max:
+            max = first
+            final = key
+    return final
+
+
+def count(list1: list[str]) -> dict[str, int]:
+    zzz: dict[str, int] = dict()
+    for key in list1:
+        if key in zzz:
+            zzz[key] += 1
+        else:
+            zzz[key] = 1
+    return zzz
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730320301"
+
+
+def test_invert() -> None:
+    """Testing invert with basic input."""
+    list1: dict[str, str] = {'apple': 'cat'}
+    assert invert(list1) == {'cat': 'apple'}
+
+
+def test_invert_again() -> None:
+    """Testing invert with more complicated input."""
+    list1: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
+    assert invert(list1) == {'b': 'a', 'd': 'c', 'f': 'e'}
+
+
+def test_invert_edge_case() -> None:
+    """Testing invert edge case when there is an empty dictionary."""
+    list1: dict[str, str] = {}
+    assert invert(list1) == {}
+
+
+def test_favorite_color() -> None:
+    """Testing favorite_color with basic input."""
+    list1: dict[str, str] = {'Sophia': 'Yellow', 'Vivian': 'Purple', 'Marc': 'Yellow'}
+    assert favorite_color(list1) == "Yellow"
+
+
+def test_favorite_color_again() -> None:
+    """Testing favorite_color with more commplicated input."""
+    list1: dict[str, str] = {'Sophia': 'Purple', 'Vivian': 'Blue', 'Marc': 'Yellow', 'Sarah': 'Purple', 'Steve': 'Purple'}
+    assert favorite_color(list1) == "Purple"
+
+
+def test_favorite_color_edge() -> None:
+    """Testing favorite_color when there is a tie."""
+    list1: dict[str, str] = {'Sophia': 'Purple', 'Vivian': 'Blue', 'Marc': 'Purple', 'Sarah': 'Blue', 'Steve': 'Green'}
+    assert favorite_color(list1) == "Purple"
+
+
+def test_count() -> None:
+    """Testing count with basic input."""
+    list1: list[str] = ['a', 'b', 'c']
+    assert count(list1) == {'a': 1, 'b': 1, 'c': 1}
+
+
+def test_count_again() -> None:
+    """Testing count with more complicated input."""
+    list1: list[str] = ['a', 'b', 'c', 'a', 'a']
+    assert count(list1) == {'a': 3, 'b': 1, 'c': 1}
+
+
+def test_count_edge() -> None:
+    """Testing count when given an empty list."""
+    list1: list[str] = []
+    assert count(list1) == {}
