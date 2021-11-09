197714673f3b48c9b1db1973fcdd4b7e474e77ba
Sophia <sophcarr@live.unc.edu>
2021-11-09 18:50:05 -0500

    progress

--- lessons/point.py
+++ lessons/point.py
@@ -17,15 +17,52 @@ class Point:
 
     def scale(self, factor: float) -> Point:
         """Pure method that does not mutate the Point."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+        return Point(self.x * factor, self.y * factor)
+
+    def __str__(self) -> str:
+        """Produce a str respresentation of a Point for humans."""
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self) -> str:
+        """Produce a str representation of a Point for Python!"""
+        return f"Point({self.x}, {self.y})"
+
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for Point * float."""
+        print("__mull__ was called")
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        """Overload the addition operator."""
+        print("__add__ was called.")
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overloading the subscription noation."""
+        if index == 0: 
+            return self.x
+        elif index == 1:
+            return self.y
+        else: 
+            raise IndexError
+
+
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
 
 
 p0: Point = Point(1.0, 2.0)
-p0.scale_by(2.0)
-print(f"{p0.x}, {p0.y}")
-
 p1: Point = p0.scale(2.0)
-print(f"{p1.x}, {p1.y}")
+print(p0)
+p1_as_a_str: str = str(p1)
+print(p1_as_a_str)
+
+p1_repr: str = repr(p1)
+print(p1_repr)
+
+print(a[0])
+print(a[1])
